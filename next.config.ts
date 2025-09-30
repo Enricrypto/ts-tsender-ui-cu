22f1de3361e557667a4faa3e30dbad84f825155a
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2025-09-30 14:21:41 +0200

    fix: next.config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-  distDir: "out",
   images: {
     unoptimized: true
   },
   basePath: "",
-  assetPrefix: "./",
   trailingSlash: true
 }
 
